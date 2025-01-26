8b7f7518e96d7d102ddc8bd0f206bb78791cf86c
Facundo Villca <facuvillca@hotmail.com>
2025-01-25 22:11:04 -0300

    2025

--- primer año/CADP/FINALES/finaFactura.pas
+++ primer año/CADP/FINALES/finaFactura.pas
@@ -13,6 +13,16 @@ vector =  array [1..dimf] of factura;
 
 procedure cargarVector(var v:vector; var diml:integer); //se dispone
 
+procedure actualizarMaximo(var maxCantSuc:integer;var maxSuc:integer;sucActual:integer;cantActual:integer);
+begin
+	if (cantActual>maxCantSuc)then
+		begin	
+			maxCantSuc:=cantActual;			//Actualizamos el maximo
+			maxSuc:=sucActual;
+		end;
+end;
+
+
 procedure procesarVector (v:vector;diml:integer);
 var
 	i:integer;
@@ -26,12 +36,15 @@ begin
 	while (i<diml)do
 		begin
 			cantActual:=0;
-			sucActual:=v[i].
-		
-		
-		
+			sucActual:=v[i].codSuc;
+			while ((i<diml) and (sucActual=v[i].codSuc)do
+				begin
+					cantActual:=cantActual+1;		//cuenta la cant de cada sucursal y luego actualiza maximo cuando cambia de suc
+					i:=i+1;
+				end;
+			actualizarMaximo(maxCantSuc,maxSuc,sucActual,cantActual);
 		end;
-
+	writeln ('La sucursal con max facturas es : ' , maxSuc); 
 end;
 
 
