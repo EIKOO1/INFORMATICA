3fe9cc5d9c4ee5a253a40ed744bc2562c457c108
EIK001 <facuvillca@hotmail.com>
2025-03-23 22:26:58 -0300

    2025

---
+++ segundo año/Primer semestre/AYED/PRACTICA/PRACTICA 1/PRACTICA 1/AYED1/src/staticClass.java
@@ -0,0 +1,21 @@
+
+public class staticClass {
+
+static void recorrerFor(int a,int b) {
+	for (int i=a;i<=b;i++) {
+		System.out.println(i);
+	}
+}
+static void recorrerWhile(int a,int b) {
+	while (a<=b) {
+		System.out.println(a++);
+	}
+}
+static void recorrerRecursivo(int a,int b) {
+	if (a<=b) {
+	System.out.println(a);
+	recorrerRecursivo(++a, b);
+		}
+	}
+}
+
